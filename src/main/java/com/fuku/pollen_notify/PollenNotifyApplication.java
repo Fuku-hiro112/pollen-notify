f5a5d940de0504b2a644e5a726588e3271ed5142
Fuku-hiro112 <hirokazu.fukuda112@gmail.com>
2026-05-19 00:53:31 +0900

    Initial commit: Spring Boot project from Spring Initializr

---
+++ src/main/java/com/fuku/pollen_notify/PollenNotifyApplication.java
@@ -0,0 +1,13 @@
+package com.fuku.pollen_notify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollenNotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollenNotifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fuku/pollen_notify/PollenNotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fuku.pollen_notify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollenNotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
